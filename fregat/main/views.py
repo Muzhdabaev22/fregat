62797b723004f8e9b59ee10106c4bab3d9299754
ediktop1gg <edik622mujpc@gmail.com>
2023-12-01 20:12:17 +0300

    update

--- fregat/main/views.py
+++ fregat/main/views.py
@@ -56,12 +56,15 @@ class HomeView(View):
 class LangView(View):
     def get(self, request, *args, **kwargs):
         form = FeedBackForm()
+        sec_form = FeedBackFormSecond()
         return render(
             request,
             'main/lang.html', context={
-                'form': form
+                'form': form,
+                'second_form': sec_form
             }
         )
+        
         
     def post(self, request, *args, **kwargs):
         
@@ -75,8 +78,8 @@ class LangView(View):
                     send_mail('С вами хотят связаться!', f"{name}. {tel}", 'edik622mujpc@gmail.com', ['edik622mujpc@gmail.com'])
                 except BadHeaderError:
                     return 
-                return HttpResponseRedirect('/')
-            return render(request, 'main/home.html', context={
+                return HttpResponseRedirect('/lang')
+            return render(request, 'main/lang.html', context={
                 'form': form,
                 'second_form': sec_form
             })
@@ -92,8 +95,8 @@ class LangView(View):
                     send_mail('С вами хотят связаться!', f"{name}. {tel}\n{comment}", 'edik622mujpc@gmail.com', ['edik622mujpc@gmail.com'])
                 except BadHeaderError:
                     return 
-                return HttpResponseRedirect('/')
-            return render(request, 'main/home.html', context={
+                return HttpResponseRedirect('/lang')
+            return render(request, 'main/lang.html', context={
                 'form': form,
                 'second_form': sec_form
             })
@@ -101,7 +104,46 @@ class LangView(View):
 
 class BlogView(View):
     def get(self, request, *args, **kwargs):
+        form = FeedBackForm()
         posts = PostBlog.objects.all()
+        sec_form = FeedBackFormSecond()
         return render(request, 'main/blog.html', context={
-            "posts": posts
+            "posts": posts,
+            'form': form,
+            'second_form': sec_form
+            })
+        
+    def post(self, request, *args, **kwargs):
+        
+        if 'first' in self.request.POST:
+            form = FeedBackForm(request.POST)
+            sec_form = FeedBackFormSecond(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                tel = form.cleaned_data['tel']
+                try:
+                    send_mail('С вами хотят связаться!', f"{name}. {tel}", 'edik622mujpc@gmail.com', ['edik622mujpc@gmail.com'])
+                except BadHeaderError:
+                    return 
+                return HttpResponseRedirect('/blog')
+            return render(request, 'main/blog.html', context={
+                'form': form,
+                'second_form': sec_form
+            })
+            
+        if 'second' in self.request.POST:
+            form = FeedBackFormSecond(request.POST)
+            sec_form = FeedBackFormSecond(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                tel = form.cleaned_data['tel']
+                comment = form.cleaned_data['comment']
+                try:
+                    send_mail('С вами хотят связаться!', f"{name}. {tel}\n{comment}", 'edik622mujpc@gmail.com', ['edik622mujpc@gmail.com'])
+                except BadHeaderError:
+                    return 
+                return HttpResponseRedirect('/blog')
+            return render(request, 'main/blog.html', context={
+                'form': form,
+                'second_form': sec_form
             })
